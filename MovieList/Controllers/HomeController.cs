156ecfb280e9d2cccfcd575a7ec5c0bfb35de4e3
SandraMuller <mursan03@gmail.com>
2015-04-30 09:45:40 +1200

    theme

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieList.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,17 @@ namespace MovieList.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("api/allmovies")]
+        public IEnumerable<Movies> GetAllMovies()
+        {
+            return db.Movies.ToList();
+        }
     }
 }
